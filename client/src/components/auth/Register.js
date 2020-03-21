f844bba8637a97f23ceb1ec42a1b264c6a495fe4
CHAKlBDEV <chakibthestorm@gmail.com>
2020-03-21 11:04:20 +0100

    finally got it working

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,10 @@
 import React, { Fragment, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { setAlert } from './../../actions/alert';
+import PropTypes from 'prop-types';
 
-const Register = () => {
+const Register = props => {
 	const [formData, updateFormData] = useState({
 		name: '',
 		email: '',
@@ -18,7 +21,7 @@ const Register = () => {
 	const onSubmit = e => {
 		e.preventDefault();
 		if (password !== password2) {
-			console.log("Passwords don't match");
+			props.setAlert("Passwords don't match", 'danger');
 		} else {
 			console.log(formData);
 		}
@@ -87,4 +90,8 @@ const Register = () => {
 	);
 };
 
-export default Register;
+Register.propTypes = {
+	setAlert: PropTypes.func.isRequired
+};
+
+export default connect(null, { setAlert })(Register);
