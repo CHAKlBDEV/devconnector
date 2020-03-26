a947f5f69d32f21a168786ecd0013ada066170fb
CHAKlBDEV <chakibthestorm@gmail.com>
2020-03-26 07:16:22 +0100

    added protected route

---
+++ client/src/components/routing/PrivateRoute.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { Route, Redirect } from 'react-router-dom';
+
+const PrivateRoute = ({ component: Component, auth, ...rest }) => {
+	return (
+		<div>
+			<Route
+				{...rest}
+				render={props =>
+					!auth.isAuthenticated && !auth.loading ? (
+						<Redirect to='/login' />
+					) : (
+						<Component {...props} />
+					)
+				}
+			/>
+		</div>
+	);
+};
+
+PrivateRoute.propTypes = {
+	auth: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+	auth: state.auth
+});
+
+export default connect(mapStateToProps, {})(PrivateRoute);
